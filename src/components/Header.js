f0a31931ca4d6196cddaf0eda5a87b2bfe8aa0dd
gastonfalena <gastonfalena@gmail.com>
2023-02-11 16:48:33 -0300

    finish

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export default function Header() {
+  return (
+    <Title>The best quote of The Simpsons</Title>
+  )
+}
+
+const Title = styled.h1`
+    color: #e76f51;
+    text-align: center;
+`
